3f77a7928a585a4bac8dfa96c46b3c20239f3a2f
Alex <AlexGorbenko57@yandex.ru>
2019-03-27 11:13:20 +0700

    Fix PlayerController

--- Assets/Scripts/Games/PlayerController.cs
+++ Assets/Scripts/Games/PlayerController.cs
@@ -7,10 +7,14 @@ namespace TestProjectAppache
 {
     public class PlayerController : MonoBehaviour
     {
-        private Platform currentPlatform;
-        [SerializeField] private BoxCollider2D playerSkin;
-        [SerializeField] private LineRenderer skinLineRenderer;
         public Action<Platform> ChangePlaftorm;
+        private Platform currentPlatform;
+        [SerializeField] private LineRenderer skinLineRenderer;
+        [SerializeField] private float AddCountHight;
+        [SerializeField] private float KoefHight;
+        [SerializeField] private float OffesetMove = 0.2f;
+
+        private float HightPlayer;
 
         public void Init(Platform startPlatform)
         {
@@ -18,10 +22,7 @@ namespace TestProjectAppache
             gameObject.transform.position = currentPlatform.PositionPlayer.position;
             gameObject.transform.SetParent(currentPlatform.PositionPlayer.transform);
             HightPlayer = 1f;
-            SizePlayer(1f);
-
-
-            //       playerSkin.transform.localScale = new Vector3(1, 0.1f, 1);
+            SizePlayer(1f);            
         }
 
         private void SizePlayer(float needPosition)
@@ -40,42 +41,41 @@ namespace TestProjectAppache
         {
             Debug.Log("Move to next platform");
             var nextPlatfrom = GameSpring.Instance.LevelGenerator.GetNextPlatform(currentPlatform);
-       //     Debug.LogError(Math.Abs(Math.Abs(nextPlatfrom.transform.position.x - currentPlatform.transform.position.x) - HightPlayer));
             if (Math.Abs(Math.Abs(nextPlatfrom.PositionPlayer.position.x - currentPlatform.PositionPlayer.position.x) - HightPlayer) < OffesetMove)
             {
-                currentPlatform = nextPlatfrom;
-                gameObject.transform.position = currentPlatform.PositionPlayer.position;
-                gameObject.transform.SetParent(currentPlatform.PositionPlayer);
                 GameSpring.Instance.AddScore(currentPlatform.Reward);
-                HightPlayer = 1;
-                SizePlayer(HightPlayer);
-                //       playerSkin.transform.localScale = new Vector3(1, 0.1f, 1);
-                if (ChangePlaftorm != null) ChangePlaftorm(currentPlatform);
+                MovePlayer(currentPlatform, nextPlatfrom);
             }
             else
             {
-                HightPlayer = 1;
-           //     playerSkin.transform.localScale = new Vector3(1, 0.1f, 1);
-                //      GameSpring.Instance.GameOver();
+                if (GameManager.Instance.IsDebug)
+                {
+                    HightPlayer = 1;
+                    SizePlayer(HightPlayer);
+                }
+                else
+                {
+                    GameSpring.Instance.GameOver();
+                }
             }
-
-           
         }
-
-
-        public float HightPlayer;
-        public float AddCountHight;
-        public float KoefHight;
-        public float OffesetMove = 0.2f;
 
         public void Stretch()
         {
-            Debug.Log("Strech press + "  + playerSkin.bounds.size.y);
+        //    Debug.Log("Strech press");
             HightPlayer += AddCountHight;
             SizePlayer(HightPlayer * KoefHight);
-            var nextPlatfrom = GameSpring.Instance.LevelGenerator.GetNextPlatform(currentPlatform);
-            Debug.LogError(Math.Abs(Math.Abs(nextPlatfrom.PositionPlayer.position.x - currentPlatform.PositionPlayer.position.x) - playerSkin.bounds.size.y));
-            // MoveToPlatform();
+        }
+
+
+        private void MovePlayer(Platform currentPlatform, Platform nextPlatfrom)
+        {
+            currentPlatform = nextPlatfrom;
+            gameObject.transform.position = currentPlatform.PositionPlayer.position;
+            gameObject.transform.SetParent(currentPlatform.PositionPlayer);
+            HightPlayer = 1;
+            SizePlayer(HightPlayer);
+            if (ChangePlaftorm != null) ChangePlaftorm(currentPlatform);
         }
     }
 }
